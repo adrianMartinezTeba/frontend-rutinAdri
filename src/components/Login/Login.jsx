e3b57563c6640431da91005c6e808d0cbe12e495
adrianMartinezTeba <adrymate@gmail.com>
2023-08-12 03:56:39 +0200

    empezando redux

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 const Login = () => {
   const [username, setUsername] = useState('');
@@ -6,7 +6,7 @@ const Login = () => {
   
   const navigate = useNavigate()
   useEffect(()=>{
-setTimeout(() => {
+setTimeo(() => {
   navigate('/home')
 }, 2000);
   },[])
